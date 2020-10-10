22a5883dc0dc9c100d6af6b4d23cb532c9bc6edd
Naveen Kumar Vadla <58026402+naveen-kumar-vadla@users.noreply.github.com>
2020-10-10 13:53:59 +0530

    created TodoItem
    current displaying using dummy data
    added css

--- src/Todo.js
+++ src/Todo.js
@@ -10,10 +10,25 @@ const Input = ({ value, onChange }) => {
   );
 };
 
+const TodoItem = ({ title, checked }) => {
+  return (
+    <div className={checked ? 'todoItem-completed' : 'todoItem-notCompleted'}>
+      <div className="highlighter">{checked}</div>
+      <div className="title">{title}</div>
+    </div>
+  )
+};
+
 class Todo extends Component {
   constructor(props) {
     super(props);
-    this.state = { items: [], inputValue: '' };
+    this.state = {
+      items: [
+        { title: 'do something', checked: true },
+        { title: 'something else', checked: false },
+      ],
+      inputValue: '',
+    };
     this.onChange = this.onChange.bind(this);
   }
 
@@ -22,9 +37,13 @@ class Todo extends Component {
   }
 
   render() {
+    const todoItems = this.state.items.map((item, i) => (
+      <TodoItem title={item.title} checked={item.checked} key={i}></TodoItem>
+    ));
     return (
       <div className='todo'>
         <h1 className='todo-name'>TODO</h1>
+        <div>{todoItems}</div>
         <Input value={this.inputValue} onChange={this.onChange} />
       </div>
     );
