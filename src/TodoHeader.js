60413b7480995a09ba4a04eb87c007d2ec0c0862
Naveen Kumar Vadla <58026402+naveen-kumar-vadla@users.noreply.github.com>
2020-10-12 20:39:54 +0530

    chanaged TodoHeader to functional component using hooks

--- src/TodoHeader.js
+++ src/TodoHeader.js
@@ -1,46 +1,27 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 import Input from './Input.js';
 
-class TodoHeader extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { isEditing: false };
-    this.toggleEditMode = this.toggleEditMode.bind(this);
-    this.resetTodo = this.resetTodo.bind(this);
-    this.updateName = this.updateName.bind(this);
-  }
-
-  toggleEditMode() {
-    this.setState(({ isEditing }) => ({ isEditing: !isEditing }));
-  }
-
-  updateName(name) {
-    this.toggleEditMode();
-    this.props.updateName(name);
-  }
-
-  resetTodo(event) {
-    event.stopPropagation();
-    this.props.reset();
-  }
-
-  render() {
-    const header = (
-      <div className='todo-name' onClick={this.toggleEditMode}>
-        {this.props.value}
-      </div>
-    );
-    const inputHeader = (
-      <Input
-        className='todo-name-edit-mode'
-        value={this.props.value}
-        onEnter={this.updateName}
-        autoFocus={true}
-      />
-    );
-    return this.state.isEditing ? inputHeader : header;
-  }
-}
+const TodoHeader = props => {
+  const [isEditing, setIsEditing] = useState(false);
+  const updateName = name => {
+    setIsEditing(false);
+    props.updateName(name);
+  };
+  const header = (
+    <div className='todo-name' onClick={() => setIsEditing(true)}>
+      {props.value}
+    </div>
+  );
+  const inputHeader = (
+    <Input
+      className='todo-name-edit-mode'
+      value={props.value}
+      onEnter={updateName}
+      autoFocus={true}
+    />
+  );
+  return isEditing ? inputHeader : header;
+};
 
 export default TodoHeader;
