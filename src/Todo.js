4fabf8b0d246bb8c3ae480e804fd3e78215b0f37
Naveen Kumar Vadla <58026402+naveen-kumar-vadla@users.noreply.github.com>
2020-10-13 18:27:20 +0530

    changed Todo to a functional component using hooks

--- src/Todo.js
+++ src/Todo.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 import TodoHeader from './TodoHeader.js';
 import TodoItem from './TodoItem.js';
@@ -8,83 +8,55 @@ import withDelete from './withDelete.js';
 
 const getNewId = () => new Date().getTime();
 
-class Todo extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { name: 'TODO', items: [] };
-    this.addTodoItem = this.addTodoItem.bind(this);
-    this.updateTodoItemState = this.updateTodoItemState.bind(this);
-    this.deleteItem = this.deleteItem.bind(this);
-    this.updateName = this.updateName.bind(this);
-    this.reset = this.reset.bind(this);
-  }
-
-  reset() {
-    const defaultState = { name: 'TODO', items: [] };
-    this.setState(_ => defaultState);
-  }
-
-  addTodoItem(title) {
-    this.setState(state => {
-      const items = state.items.slice();
-      const id = getNewId();
-      items.push({ id, title, state: State.Created });
-      return { items };
-    });
-  }
-
-  updateName(name) {
-    this.setState(_ => ({ name }));
-  }
-
-  deleteItem(id) {
-    this.setState(({ items }) => ({
-      items: items.filter(item => item.id !== id),
-    }));
-  }
-
-  updateTodoItemState(id) {
-    this.setState(state => {
-      const items = state.items.slice();
-      const index = items.findIndex(item => item.id === id);
-      const item = Object.assign({}, items[index]);
-      item.state = item.state.next;
-      items[index] = item;
-      return { items };
-    });
-  }
-
-  createTodoItems() {
-    return this.state.items.map(item => {
+const Todo = () => {
+  const [name, setName] = useState('TODO');
+  const [items, setItems] = useState([]);
+  const addItem = title => {
+    const itemsCopy = items.slice();
+    const id = getNewId();
+    itemsCopy.push({ id, title, state: State.Created });
+    setItems(itemsCopy);
+  };
+  const updateItemState = id => {
+    const itemsCopy = items.slice();
+    const item = itemsCopy.find(item => item.id === id);
+    item.state = item.state.next;
+    setItems(itemsCopy);
+  };
+  const deleteItem = id => setItems(items.filter(item => item.id !== id));
+  const updateName = name => setName(name);
+  const reset = () => {
+    setName('TODO');
+    setItems([]);
+  };
+  const createItems = () => {
+    return items.map(item => {
       const DeletableTodoItem = withDelete(TodoItem);
       return (
         <DeletableTodoItem
           id={item.id}
-          deleteMethod={this.deleteItem}
+          deleteMethod={deleteItem}
           state={item.state}
           title={item.title}
           key={item.id}
-          onClick={this.updateTodoItemState}
+          onClick={updateItemState}
         />
       );
     });
-  }
-
-  render() {
-    const DeletableTodoHeader = withDelete(TodoHeader);
-    return (
-      <div className='todo'>
-        <DeletableTodoHeader
-          id={0}
-          value={this.state.name}
-          updateName={this.updateName}
-          deleteMethod={this.reset}
-        />
-        {this.createTodoItems()}
-        <Input onEnter={this.addTodoItem} value='' />
-      </div>
-    );
-  }
-}
+  };
+  const DeletableTodoHeader = withDelete(TodoHeader);
+  return (
+    <div className='todo'>
+      <DeletableTodoHeader
+        id={0}
+        value={name}
+        updateName={updateName}
+        deleteMethod={reset}
+      />
+      {createItems()}
+      <Input className='todo-item-adder' onEnter={addItem} value='' />
+    </div>
+  );
+};
 
 export default Todo;
