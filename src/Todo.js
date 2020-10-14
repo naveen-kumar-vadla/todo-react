b35408cd02081953080875f2c3daf390ace8a67b
Naveen Kumar Vadla <58026402+naveen-kumar-vadla@users.noreply.github.com>
2020-10-14 23:33:38 +0530

    changed passing prop key to updateState  form onClick
    it improves readability

--- src/Todo.js
+++ src/Todo.js
@@ -24,7 +24,7 @@ const Todo = () => {
           {...item}
           deleteMethod={deleteItem}
           key={item.id}
-          onClick={updateItemState}
+          updateState={updateItemState}
         />
       );
     });
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -3,7 +3,7 @@ import React from 'react';
 const TodoItem = props => {
   const className = `todoItem-${props.state}`;
   return (
-    <div className={className} onClick={() => props.onClick(props.id)}>
+    <div className={className} onClick={() => props.updateState(props.id)}>
       <div className='highlighter'></div>
       <div className='title'> {props.title} </div>
     </div>
